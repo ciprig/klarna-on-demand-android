eb45daf4506fba52aa941bc7b4aa5be54f3b9400
ovidiuMaier <maier.ioan.ovidiu@gmail.com>
2019-03-06 15:02:05 +0200

    Handle bankid redirect

--- klarna-on-demand/src/main/java/com/klarna/ondemand/WebViewActivity.java
+++ klarna-on-demand/src/main/java/com/klarna/ondemand/WebViewActivity.java
@@ -29,6 +29,7 @@ abstract class WebViewActivity extends Activity {
     private static final String USER_ERROR_EVENT_IDENTIFIER = "userError";
     private static final String OPEN_IN_BROWSER_PARAMETER = "openInBrowser=true";
     private static final String BANKID_INTENT = "scheme=bankid";
+    private static final String BANKID_REDIRECT = "bankid:///";
     public static final int RESULT_ERROR = 1;
 
     @Override
@@ -100,7 +101,7 @@ abstract class WebViewActivity extends Activity {
 
                     return true;
                 }
-                else if(url.contains(BANKID_INTENT)) {
+                else if(url.contains(BANKID_INTENT) || url.contains(BANKID_REDIRECT)) {
                     Intent intent = new Intent();
                     intent.setPackage("com.bankid.bus");
                     intent.setAction(Intent.ACTION_VIEW);
